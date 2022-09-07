e7fc66dc3841ee15728118761d90e9e71a139782
stylelinz_mac <cwchuang1997@gmail.com>
2022-09-07 10:08:47 +0800

    update libs

--- libs/data/src/lib/data.ts
+++ libs/data/src/lib/data.ts
@@ -2,3 +2,22 @@ export type Todo = {
   message: string;
   done: boolean;
 };
+
+export const shareTodos = (): Todo[] => {
+  return [
+    { message: 'survey nx', done: false },
+    { message: 'production update', done: false },
+  ];
+};
+
+export const createShareTodos = (...args: (Todo | Todo[])[]): Todo[] => {
+  const result: Todo[] = [];
+  for (const todo of args) {
+    if (Array.isArray(todo)) {
+      result.push(...todo);
+    } else {
+      result.push(todo);
+    }
+  }
+  return result;
+};
