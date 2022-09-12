f4742cef2f2e0d19b72e3aae37927c08293cdbe0
stylelinz_mac <cwchuang1997@gmail.com>
2022-09-12 10:46:43 +0800

    update random

--- apps/random/src/main.ts
+++ apps/random/src/main.ts
@@ -2,4 +2,7 @@ import { generateRandomId } from '@nest-nx/utils/helpers';
 
 console.log('Hello World!');
 const id = generateRandomId();
-console.log(`${id}, Hi there.`);
+const now = new Date();
+const formattedNow = Intl.DateTimeFormat('zh-TW').format(now);
+console.log(`${id}, Hi there.
+It's ${formattedNow}`);
